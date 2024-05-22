7b275ef21cb18ccf1c325c04098b2842061de200
MahardiikaKD <dikamahar884@gmail.com>
2024-05-22 20:24:30 +0700

    init

---
+++ prisma/migrations/20240522132239_create_table_users/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` VARCHAR(100) NOT NULL,
+    `username` VARCHAR(100) NOT NULL,
+    `password` VARCHAR(100) NOT NULL,
+    `name` VARCHAR(100) NOT NULL,
+    `email` VARCHAR(100) NOT NULL,
+    `token` VARCHAR(100) NULL,
+
+    UNIQUE INDEX `users_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
